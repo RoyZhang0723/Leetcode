7ee3fbd1b5224c6fccf2d6d06f43bcb96c9ad055
Zhang Xinyu <zhangxinyushuai@gamil.com>
2021-07-16 15:27:33 +0800

    最长回文子串

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 最长回文子串
@@ -91,6 +92,36 @@ public class LongestPalindrome {
         }
         return s.substring(begin, begin + maxLen);
     }
+
+    /**
+     *解法三： 中心线枚举
+     * @param s
+     * @return
+     */
+    public static String longestPalindromeCentral(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R-L-1;
+    }
+
 
     public static boolean isPalindrome(String str, int start, int end) {
         if (end - start + 1 == 2) {

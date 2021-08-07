1fba2a59d247cbca786f194e5982c71952574582
Zhang Xinyu <zhangxinyushuai@gamil.com>
2021-08-07 15:01:32 +0800

    二叉树中所有距离为 K 的结点

--- src/DistanceK.java
+++ src/DistanceK.java
@@ -1,95 +1,60 @@
-import sun.misc.Queue;
-
 import java.util.*;
 
+/**
+ * 二叉树中所有距离为 K 的结点
+ *
+ * https://www.lintcode.com/problem/1506/
+ *
+ * 将树转化为图再bfs逐层找距离为K的结点
+ *
+ */
 public class DistanceK {
-    static Map<BinaryTreeNode, BinaryTreeNode> parent = new HashMap<>();
-    static Queue<BinaryTreeNode> binaryTreeNodeQueue = new Queue<>();
+    static Map<TreeNode, TreeNode> parent = new HashMap<>();
+    // 注意在这里初始化的时候，要用LinkedList而不能用ArrayDeque(添加null的时候会报错)
+    static Queue<TreeNode> treeNodeQueue = new LinkedList<>();
     static List<Integer> ansList = new ArrayList<>();
-
     /**
-     * 二叉树中所有距离为k的结点
-     * 给定一个二叉树（具有根结点root），一个目标结点target，和一个整数值 K 。
-     * <p>
-     * 返回到目标结点 target 距离为 K 的所有结点的值的列表。 答案可以以任何顺序返回。
-     * <p>
-     * 来源：力扣（LeetCode）
-     * 链接：https://leetcode-cn.com/problems/all-nodes-distance-k-in-binary-tree
+     * @param root: the root of the tree
+     * @param target: the target
+     * @param K: the given K
+     * @return: All Nodes Distance K in Binary Tree
      */
-    public static void main(String[] args) throws InterruptedException {
-        /**
-         * binaryTreeNode1(root)
-         */
-        BinaryTreeNode binaryTreeNode1 = new BinaryTreeNode(3);
-        BinaryTreeNode binaryTreeNode2 = new BinaryTreeNode(5);
-        BinaryTreeNode binaryTreeNode3 = new BinaryTreeNode(1);
-        BinaryTreeNode binaryTreeNode4 = new BinaryTreeNode(6);
-        BinaryTreeNode binaryTreeNode5 = new BinaryTreeNode(2);
-        BinaryTreeNode binaryTreeNode6 = new BinaryTreeNode(0);
-        BinaryTreeNode binaryTreeNode7 = new BinaryTreeNode(8);
-        BinaryTreeNode binaryTreeNode8 = new BinaryTreeNode(7);
-        BinaryTreeNode binaryTreeNode9 = new BinaryTreeNode(4);
-        binaryTreeNode1.leftNode = binaryTreeNode2;
-        binaryTreeNode1.rightNode = binaryTreeNode3;
-        binaryTreeNode2.leftNode = binaryTreeNode4;
-        binaryTreeNode2.rightNode = binaryTreeNode5;
-        binaryTreeNode5.leftNode = binaryTreeNode8;
-        binaryTreeNode5.rightNode = binaryTreeNode9;
-        binaryTreeNode3.leftNode = binaryTreeNode6;
-        binaryTreeNode3.rightNode = binaryTreeNode7;
-        List<Integer> ansL = distanceK(binaryTreeNode1, binaryTreeNode2, 2);
-        for (Integer integer : ansL) {
-            System.out.println(integer);
-        }
-    }
-
-    /**
-     * 先将二叉树变成图
-     * 然后从target开始进行bfs（以后记住bfs就是用队列实现的，用bfs必用到队列）
-     * 核心在于通过加入null的方法，对该图的层级进行分割
-     * @param root
-     * @param target
-     * @param K
-     * @return
-     * @throws InterruptedException
-     */
-    public static List<Integer> distanceK(BinaryTreeNode root, BinaryTreeNode target, int K) throws InterruptedException {
-        BinaryTreeNode n;
+    public static List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        TreeNode n;
         dfs(root, null);
-        binaryTreeNodeQueue.enqueue(null);
-        binaryTreeNodeQueue.enqueue(target);
-        Set<BinaryTreeNode> seen = new HashSet<>();
+        treeNodeQueue.offer(null);
+        treeNodeQueue.offer(target);
+        // 防止在回溯到par的时候再走回头路
+        Set<TreeNode> seen = new HashSet<>();
         seen.add(target);
         seen.add(null);
         int dist = 0;
-        while (!binaryTreeNodeQueue.isEmpty()) {
-            BinaryTreeNode node = binaryTreeNodeQueue.dequeue();
+        while (!treeNodeQueue.isEmpty()) {
+            TreeNode node = treeNodeQueue.poll();
             if (node == null) {
                 if (dist == K) {
-                    while (!binaryTreeNodeQueue.isEmpty()) {
-                        n = binaryTreeNodeQueue.dequeue();
-                        ansList.add(n.value);
+                    while (!treeNodeQueue.isEmpty()) {
+                        n = treeNodeQueue.poll();
+                        ansList.add(n.val);
                     }
                     return ansList;
                 }
-                /**
-                 * 这里的null就是为了把该二叉树的每一个层级分开，方便记录距离
-                 */
-                binaryTreeNodeQueue.enqueue(null);
+                // 这里的null是用来分层的
+                treeNodeQueue.offer(null);
                 dist++;
             } else {
-                if (!seen.contains(node.leftNode)) {
-                    seen.add(node.leftNode);
-                    binaryTreeNodeQueue.enqueue(node.leftNode);
+                if (!seen.contains(node.left)) {
+                    seen.add(node.left);
+                    treeNodeQueue.offer(node.left);
                 }
-                if (!seen.contains(node.rightNode)) {
-                    seen.add(node.rightNode);
-                    binaryTreeNodeQueue.enqueue(node.rightNode);
+                if (!seen.contains(node.right)) {
+                    seen.add(node.right);
+                    treeNodeQueue.offer(node.right);
                 }
-                BinaryTreeNode par = parent.get(node);
+                TreeNode par = parent.get(node);
                 if (!seen.contains(par)) {
                     seen.add(par);
-                    binaryTreeNodeQueue.enqueue(par);
+                    treeNodeQueue.offer(par);
                 }
             }
         }
@@ -97,15 +62,40 @@ public class DistanceK {
     }
 
     /**
-     * 该方法将所有的结点的父节点记录到map里，这样就成了双向连接，成为图了
+     * 把每个节点的父节点记录在map里面，这样就讲树变成图了
      * @param node
      * @param par
      */
-    public static void dfs(BinaryTreeNode node, BinaryTreeNode par) {
+    public static void dfs(TreeNode node, TreeNode par) {
         if (node != null) {
             parent.put(node, par);
-            dfs(node.leftNode, node);
-            dfs(node.rightNode, node);
+            dfs(node.left, node);
+            dfs(node.right, node);
         }
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(3);
+        TreeNode treeNode2 = new TreeNode(5);
+        TreeNode treeNode3 = new TreeNode(1);
+        TreeNode treeNode4 = new TreeNode(6);
+        TreeNode treeNode5 = new TreeNode(2);
+        TreeNode treeNode6 = new TreeNode(0);
+        TreeNode treeNode7 = new TreeNode(8);
+        TreeNode treeNode8 = new TreeNode(7);
+        TreeNode treeNode9 = new TreeNode(4);
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+        treeNode2.left = treeNode4;
+        treeNode2.right = treeNode5;
+        treeNode5.left = treeNode8;
+        treeNode5.right = treeNode9;
+        treeNode3.left = treeNode6;
+        treeNode3.right = treeNode7;
+        List<Integer> ansList = distanceK(treeNode1, treeNode2, 2);
+//        Queue<TreeNode> treeNodeQueue = new ArrayDeque<>();
+//        treeNodeQueue.offer(null);
+//        TreeNode a = new TreeNode(3);
+//        treeNodeQueue.offer(a);
     }
 }

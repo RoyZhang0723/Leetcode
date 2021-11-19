21d48071d0688a503561398d63a4dc75c8c7342c
Zhang Xinyu <zhangxinyushuai@gamil.com>
2021-11-19 15:22:55 +0800

    编辑距离（非常好的一道动态规划问题）

---
+++ src/MinDistance.java
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode-cn.com/problems/edit-distance/submissions/
+ * 编辑距离（非常好的一道动态规划问题）
+ */
+public class MinDistance {
+    public static int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        // base case
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // 针对每一个不同的字符，有三种修改方式， 删除，替换，插入。直接枚举这三种修改方法，选出其中方法数最少的
+                    dp[i][j] = min(dp[i - 1][j] + 1, dp[i - 1][j - 1] + 1, dp[i][j - 1] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minDistance("rad", "apple"));
+    }
+}

89b13857881342f5df1e9c86a7f2da403e78e8d2
Zhang Xinyu <zhangxinyushuai@gmail.com>
2023-08-06 14:09:36 +0800

    更新

--- src/test.java
+++ src/test.java
@@ -1,6 +1,9 @@
 public class test {
     public static void main(String[] args) {
-        int a = 0;
-        System.out.println(a++);
+       String s = "hello";
+       StringBuilder sb = new StringBuilder(s);
+       sb.setCharAt(2, 'n');
+       s = sb.toString();
+       System.out.println(s);
     }
 }
